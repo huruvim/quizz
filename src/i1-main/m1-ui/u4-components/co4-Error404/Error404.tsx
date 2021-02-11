6aa548dc6dfb081ff02b9c44be467595e2453a2b
Erzhena <erzheena8@icloud.com>
2021-02-11 13:54:08 +0700

    registration page

--- src/i1-main/m1-ui/u4-components/co4-Error404/Error404.tsx
+++ src/i1-main/m1-ui/u4-components/co4-Error404/Error404.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import s from './Error404.module.css'
-import girl from '../../u1-common/c1-assets/images/girl-dancer.gif'
+// import girl from '../../u1-common/c1-assets/images/girl-dancer.gif'
 
 function Error404() {
     return (
         <div className={s.error404}>
             <div className={s.error404_text}>404
             Page not found!</div><br/>
-            <div className={s.girl}><img src={girl} alt=""/>
+            <div className={s.girl}><img  alt=""/>
             </div>
         </div>
     )
--- src/i1-main/m3-dal/api.ts
+++ src/i1-main/m3-dal/api.ts
@@ -1,2 +1,34 @@
-// here will be api
-export const k = 1;
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: 'http://localhost:7542/2.0/',
+    withCredentials: true
+})
+
+export type RequestType = {
+    email: string
+    password: string
+}
+
+type ResponseType = {
+    data: any
+    status: number
+    statusText: string
+    error?: string
+
+}
+
+export const APIRegistration = {
+    signUp(data: RequestType) {
+        return instance.post<ResponseType>('auth/register', data)
+            .then(res =>{
+                return res
+            })
+    },
+    ping() {
+        return instance.get('ping')
+            .then(res =>{
+                return res
+            })
+    }
+}
