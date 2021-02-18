0e6773449560655a53feac13a4ed534932219254
Valentyn-999 <valentyn.333k@gmail.com>
2021-02-19 00:25:58 +0200

    just before experimental commit

--- src/i2-features/f2-Table/t2-Cards/Cards.tsx
+++ src/i2-features/f2-Table/t2-Cards/Cards.tsx
@@ -1,10 +1,10 @@
-import React, {ChangeEvent, useContext, useEffect, useRef, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../../i1-main/m2-bll/store";
 import {CardPacksType, RespondCardType} from "../../../i1-main/m3-dal/api";
 import {addPackTC, getPacksTC} from "../t1-Packs/packs-reducer";
 import {NavLink, Redirect} from "react-router-dom";
-import {Button, Form, FormInstance, Input, Layout, Modal, Popconfirm, Space, Table} from "antd";
+import {Button, Input, Layout, Modal, Popconfirm, Space, Table} from "antd";
 import {Content} from "antd/es/layout/layout";
 import {addCardTC, currentPackIdAC, deleteCardTC, getCardsTC, InitialStateType, updateCardTC} from "./cards-reducer";
 import {PATH} from "../../../i1-main/m1-ui/u3-routes/Routes";
@@ -83,103 +83,6 @@ export const Cards = () => {
         debugger
     }
 
-
-
-    const EditableContext = React.createContext<FormInstance<any> | null>(null);
-
-    interface Item {
-        key: string;
-        name: string;
-        age: string;
-        address: string;
-    }
-
-    interface EditableRowProps {
-        index: number;
-    }
-
-    const EditableRow: React.FC<EditableRowProps> = ({ index, ...props }) => {
-        const [form] = Form.useForm();
-        return (
-            <Form form={form} component={false}>
-                <EditableContext.Provider value={form}>
-                    <tr {...props} />
-                </EditableContext.Provider>
-            </Form>
-        );
-    };
-
-    interface EditableCellProps {
-        title: React.ReactNode;
-        editable: boolean;
-        children: React.ReactNode;
-        dataIndex: keyof Item;
-        record: Item;
-        handleSave: (record: Item) => void;
-    }
-
-    const EditableCell: React.FC<EditableCellProps> = ({
-                                                           title,
-                                                           editable,
-                                                           children,
-                                                           dataIndex,
-                                                           record,
-                                                           handleSave,
-                                                           ...restProps
-                                                       }) => {
-        const [editing, setEditing] = useState(false);
-        const inputRef = useRef<Input>(null);
-        const form = useContext(EditableContext)!;
-
-        useEffect(() => {
-            if (editing) {
-                inputRef.current!.focus();
-            }
-        }, [editing]);
-
-        const toggleEdit = () => {
-            setEditing(!editing);
-            form.setFieldsValue({ [dataIndex]: record[dataIndex] });
-        };
-
-        const save = async () => {
-            try {
-                const values = await form.validateFields();
-
-                toggleEdit();
-                handleSave({ ...record, ...values });
-            } catch (errInfo) {
-                console.log('Save failed:', errInfo);
-            }
-        };
-
-        let childNode = children;
-
-        if (editable) {
-            childNode = editing ? (
-                <Form.Item
-                    style={{ margin: 0 }}
-                    name={dataIndex}
-                    rules={[
-                        {
-                            required: true,
-                            message: `${title} is required.`,
-                        },
-                    ]}
-                >
-                    <Input ref={inputRef} onPressEnter={save} onBlur={save} />
-                </Form.Item>
-            ) : (
-                <div className="editable-cell-value-wrap" style={{ paddingRight: 24 }} onClick={toggleEdit}>
-                    {children}
-                </div>
-            );
-        }
-
-        return <td {...restProps}>{childNode}</td>;
-    };
-
-    type EditableTableProps = Parameters<typeof Table>[0]
 
     const columns: ColumnsType<User> = [
         {
